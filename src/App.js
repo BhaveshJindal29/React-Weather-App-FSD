ccb1af4bf2112c569b6934e00b9429e92610c96e
BhaveshJindal29 <bhavesh.jindal29@gmail.com>
2022-05-03 17:05:19 +0530

    Replaced Functional components with class components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HeadingSection from './components/Header/HeadingSection';
 import SearchSection from './components/Search/SearchSection';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className='mainSection'>
-      <HeadingSection />
-      <SearchSection />
-    </div>
-  );
+class App extends Component {
+
+  render(){
+    return (
+      <div className='mainSection'>
+        <HeadingSection />
+        <SearchSection />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- src/components/Search/SearchSection.js
+++ src/components/Search/SearchSection.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React,{Component} from 'react'
 import './SearchSection.css';
 
-function SearchSection() {
-  return (
-    <div className='searchMain'>
-        <input className="locationTextBox" type="text" id="cityName" size="75" placeholder='Enter City Name...'/>
-        <input className="searchButton" type="submit" value="Search"/>
-    </div>
-  )
+class SearchSection extends Component {
+
+    render(){
+        return (
+            <div>
+                <div className='searchMain'>
+                      <input className="locationTextBox" type="text" id="cityName" size="75" placeholder='Enter City Name...'/>
+                      <input className="searchButton" type="submit" value="Search"/><br/>
+                  </div>
+            </div>
+          
+        )
+    }
+  
 }
 
 export default SearchSection
