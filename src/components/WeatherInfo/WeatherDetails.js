ca099accd83bf89d7599d1be81d10e1ac7b606cb
BhaveshJindal29 <bhavesh.jindal29@gmail.com>
2022-05-08 16:04:06 +0530

    Updated API code

--- src/components/WeatherInfo/WeatherDetails.js
+++ src/components/WeatherInfo/WeatherDetails.js
@@ -4,12 +4,10 @@ import './WeatherDetails.css';
 export class WeatherDetails extends Component {
   constructor(props) {
     super(props);
-
+    
   }
 
-  // constructor() {
-  //   super();
-  // }
+
 
   render() {
     let dateNow = new Date();
@@ -18,7 +16,7 @@ export class WeatherDetails extends Component {
       <div>
         <div className="WeatherDetailsMain">
           <span className="cityLocation">
-            {this.props.cityName}, {this.props.cityCountry},{" "}{dateNow.toDateString()}
+          {this.props.cityName}, {this.props.cityCountry},  {dateNow.toDateString()} 
           </span>
           <br />
 
@@ -26,20 +24,26 @@ export class WeatherDetails extends Component {
             <div className="leftSide">
               <h1>
                 <span className="weatherIcon">
-                  {this.props.cityTemperature}&deg;C <img src={weatherIcon} />
+                {this.props.cityTemperature}&deg;C               <img src={weatherIcon} />
                 </span>
               </h1>
             </div>
             <div className="rightSide">
               <p className="weatherDetailsIndividual">
-                Weather: <b>{this.props.cityWeatherDescription}</b><br />
-                Wind: <b>{this.props.cityWind} km/hr</b><br /><br />
-                Humidity: <b>{this.props.cityHumidity}%</b> <br />
-                Pressure: <b> {this.props.cityPressure} Pa</b><br /><br />
-                Max Temp: <b>{this.props.cityMaxTmp}&deg;C </b><br />
-                Min Temp: <b>{this.props.cityMinTmp}&deg;C</b><br /> <br />
-                Sunrise : <b> {this.props.citySunrise}</b><br />
-                Sunset: <b> {this.props.citySunset}</b><br />
+              Weather: <b>{this.props.cityWeatherDescription}</b><br/>
+         Wind:  <b>{this.props.cityWind} km/hr</b> 
+        <br/>
+        <br/>
+        Humidity: <b>{this.props.cityHumidity}%</b> <br/>
+        Pressure: <b> {this.props.cityPressure} Pa</b>
+       <br/>
+       <br/>
+         Max Temp: <b>{this.props.cityMaxTmp}&deg;C </b><br/>
+         Min Temp: <b>{this.props.cityMinTmp}&deg;C</b>
+        <br/> <br/>
+        Sunrise : <b> {this.props.citySunrise}</b><br/>
+        Sunset: <b> {this.props.citySunset}</b>
+       <br/>
               </p>
             </div>
           </div>
